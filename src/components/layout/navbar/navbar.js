f84dc59c88a92c111dbef7cefee49cc45722cc6e
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2023-08-01 22:41:01 -0300

    Criado page

--- src/components/layout/navbar/navbar.js
+++ src/components/layout/navbar/navbar.js
@@ -1,15 +1,21 @@
 import React from "react";
-import './navbar.module.css';
+
+import styles from './navbar.module.css';
 
 import {Link} from 'react-router-dom';
+
+import logo from '../../../img/icons8-person-80.png';
 
 function Navbar(){
     return(
         <>
-            <section>
-                <nav>
-                    <Link to="/">logo</Link>
-                    <Link to="/">Home</Link>
+            <section >
+                <nav className={styles.navbar}>
+                    <Link to="/" className={styles.navLink}>
+                        <img className={styles.logo} src={ logo } alt="Logo"/>
+                        <span className={styles.titleNamePage}>Page</span>
+                    </Link>
+                    <Link to="/" className={styles.navItem}>Home</Link>
                 </nav>
             </section>
         </>
